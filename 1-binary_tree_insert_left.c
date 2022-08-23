ce2030d008aa9652a1b4b680d6ca5950b447a9fc
GetacherD <getacher.d@gmail.com>
2022-08-23 13:14:03 +0300

    use brace

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,7 +11,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *Node;
 
 	Node = malloc(sizeof(binary_tree_t));
-	if (!Node || !parent)
+	if ((!Node) || (!parent))
 		return (NULL);
 	Node->left = NULL;
 	Node->right = NULL;
@@ -21,10 +21,9 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		parent->left = Node;
 	else
 	{
-		parent->left->parent = Node;
+		(parent->left)->parent = Node;
 		Node->left = parent->left;
 		parent->left = Node;
 	}
 	return (Node);
 }
-
