0af0333aa71a9b87aa11db76fe095057cd9912b3
GetacherD <getacher.d@gmail.com>
2022-08-23 15:37:15 +0300

    count only leaves

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -2,13 +2,20 @@
 /**
  * binary_tree_leaves - count number of leafs of node
  * @tree: node whose tree required
- * Return: number of leafs
+ * Return: number of leafs -> do not count root
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
+	size_t leaves = 0;
+
 	if (tree == NULL)
 		return (0);
-	if (tree->left  == NULL || tree->right == NULL)
-		return (1);
-	return (2);
+	if (tree)
+	{
+		if (tree->left == NULL && tree->right == NULL)
+			leaves++;
+		leaves += binary_tree_leaves(tree->left);
+		leaves += binary_tree_leaves(tree->right);
+	}
+	return (leaves);
 }
