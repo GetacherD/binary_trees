99d7dc40bd88f22691cbdb069be84abca1cc3c24
GetacherD <getacher.d@gmail.com>
2022-08-23 12:58:25 +0300

    fix child parent

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -21,6 +21,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		parent->left = Node;
 	else
 	{
+		parent->left->parent = Node;
 		Node->left = parent->left;
 		parent->left = Node;
 	}
