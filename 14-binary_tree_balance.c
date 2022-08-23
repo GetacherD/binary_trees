d967fc65be734b8af37c013c4c8c1891d18fe8d1
GetacherD <getacher.d@gmail.com>
2022-08-23 16:46:18 +0300

    fix height

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,18 +1,29 @@
 #include "binary_trees.h"
 /**
- * binary_tree_balance - get balance of binary tree
+ * balance - get balance of binary tree
  * @tree: root of binary tree
  * Return: balance of node
  */
-int binary_tree_balance(const binary_tree_t *tree)
+int balance(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
 	if (tree->left && tree->right == NULL)
-		return (1);
-	if (tree->left == NULL && tree->right)
-		return (-1);
+		return (1 + balance(tree->right) + balance(tree->left));
 	if (tree->left == NULL && tree->right == NULL)
-		return (0);
-	return (2);
+		return (balance(tree->left) + balance(tree->right));
+	if (tree->left == NULL && tree->right)
+		return (-1 + balance(tree->left) + balance(tree->right));
+	return (balance(tree->left) + balance(tree->right));
+}
+/**
+ * binary_tree_balance - get balance including  height
+ * @tree: root node
+ * Return: balance of node
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	return (balance(tree)
+		+ binary_tree_height(tree->left)
+		- binary_tree_height(tree->right));
 }
