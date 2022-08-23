fa1024aebf81cc717b175eddda95e785b2f54415
GetacherD <getacher.d@gmail.com>
2022-08-23 13:22:02 +0300

    return child of child

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,19 +11,21 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *Node;
 
 	Node = malloc(sizeof(binary_tree_t));
-	if ((!Node) || (!parent))
+	if (!Node)
+		return (NULL);
+	if (!parent)
 		return (NULL);
 	Node->left = NULL;
 	Node->right = NULL;
 	Node->parent = parent;
 	Node->n = value;
-	if (!(parent->left))
-		parent->left = Node;
-	else
+	if (parent->left)
 	{
 		(parent->left)->parent = Node;
 		Node->left = parent->left;
 		parent->left = Node;
+		return (Node->left);
 	}
+	parent->left = Node;
 	return (Node);
 }
