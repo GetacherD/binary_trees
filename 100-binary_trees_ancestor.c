f986015084ba0602e89ad809ad86ad2e2c11bd71
GetacherD <getacher.d@gmail.com>
2022-08-24 07:19:12 +0300

    get ancestor

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -19,6 +19,72 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 	}
 	return (depth);
 }
+/**
+ * rightup - move right tree up
+ * @first: left node
+ * @second: right node
+ * @depth1: depth of first node
+ * Return: common ancesstor
+ */
+binary_tree_t *rightup(const binary_tree_t *first,
+		const binary_tree_t *second, size_t depth1)
+{
+	binary_tree_t *par1, *par2;
+	size_t depth2;
+
+	par2 = second->parent;
+	depth2 = binary_tree_depth(par2);
+	while (depth1 != depth2)
+	{
+		par2 = par2->parent;
+		depth2 = binary_tree_depth(par2);
+	}
+	if (par2 == first)
+		return (par2);
+	par1 = first->parent;
+	par2 = par2->parent;
+	while (par1 && par2 && (par1 != par2))
+	{
+		par1 = par1->parent;
+		par2 = par2->parent;
+	}
+	if (par1 && par2 && (par1 == par2))
+		return (par1);
+	return (NULL);
+}
+/**
+ * leftup - move left node up
+ * @first: left node
+ * @second: right node
+ * @depth2: depth of second node
+ * Return: common ancestor
+ */
+binary_tree_t *leftup(const binary_tree_t *first,
+		const binary_tree_t *second, size_t depth2)
+{
+	binary_tree_t *par1, *par2;
+	size_t depth1;
+
+	par1 = first->parent;
+	depth1 = binary_tree_depth(par1);
+	while (depth1 != depth2)
+	{
+		par1 = par1->parent;
+		depth1 = binary_tree_depth(par1);
+	}
+	if (par1 == second)
+		return (par1);
+	par1 = par1->parent;
+	par2 = second->parent;
+	while (par1 && par2 && (par1 != par2))
+	{
+		par1 = par1->parent;
+		par2 = par2->parent;
+	}
+	if (par1 && par2 && (par1 == par2))
+		return (par1);
+	return (NULL);
+}
 
 /**
  * binary_trees_ancestor - get common ancestor
@@ -34,30 +100,22 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 
 	if (!first || !second)
 		return (NULL);
-	par1 = first->parent;
-	par2 = second->parent;
-	if (par1 == second)
-		return (par1);
-	if (par2 == first)
-		return (par2);
 	depth1 = binary_tree_depth(first);
 	depth2 = binary_tree_depth(second);
-	while (depth1 < depth2)
-	{
-		depth2 = binary_tree_depth(par2);
-		par2 = par2->parent;
-	}
-	while (depth2 < depth1)
-	{
-		depth1 = binary_tree_depth(par1);
-		par1 = par1->parent;
-	}
+	if (depth1 < depth2)
+		return (rightup(first, second, depth1));
+	else if (depth2 < depth1)
+		return (leftup(first, second, depth2));
+	if (first == second)
+		return ((binary_tree_t *)first);
+	par1 = first->parent;
+	par2 = second->parent;
 	while (par1 && par2 && (par1 != par2))
 	{
 		par1 = par1->parent;
 		par2 = par2->parent;
 	}
-	if (par1 && par2 && (par1 == par2 || first == par2 || second == par1))
+	if (par1 && par2 && (par1 == par2))
 		return (par1);
 	return (NULL);
 }
