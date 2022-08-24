fb8b94eeffe155f33fdd879fc7bcd301aad43b94
GetacherD <getacher.d@gmail.com>
2022-08-24 10:14:05 +0300

    rotate left

---
+++ 103-binary_tree_rotate_left.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_rotate_left - rotate left the root
+ * @tree: root node
+ * Return: new root node
+ */
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
+{
+	binary_tree_t *right;
+
+	if (!tree)
+		return (NULL);
+	if (!tree->right)
+		return (tree);
+
+	right = tree->right;
+	right->parent = NULL;
+	tree->parent = right;
+	if (!tree->left)
+	{
+		tree->left = right->left;
+		tree->right = NULL;
+	}
+	else
+		tree->right = right->left;
+	right->left = tree;
+	return (right);
+}
