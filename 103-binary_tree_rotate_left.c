d25137339ea1b05b2129eb0ace8bb45fa88cd19f
GetacherD <getacher.d@gmail.com>
2022-08-24 12:16:14 +0300

    modify parent

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -19,6 +19,8 @@ binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 	if (!tree->left)
 	{
 		tree->left = root->left;
+		if (tree->left)
+			tree->left->parent = tree;
 		tree->right = NULL;
 	}
 	else
